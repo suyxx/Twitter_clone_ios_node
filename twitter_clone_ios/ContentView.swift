b78e0cad4408bb9b9e437ed2b5491206b46fbd14
Suyash Saurabh <suyashsaurabh10@gmail.com>
2023-01-16 19:51:13 +0530

    Initial Commit

---
+++ twitter_clone_ios/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  twitter_clone_ios
+//
+//  Created by Suyash Saurabh on 16/01/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ twitter_clone_ios/twitter_clone_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitter_clone_iosApp.swift
+//  twitter_clone_ios
+//
+//  Created by Suyash Saurabh on 16/01/23.
+//
+
+import SwiftUI
+
+@main
+struct twitter_clone_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
